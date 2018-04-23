21f804077b29cb3dc3f5c2e5495fe5c2d624848e
Sahil Patel <sahil-mac@Sahils-MacBook-Pro.local>
2018-04-23 22:43:01 +0530

    added code for adding SearchView

--- app/src/main/java/com/example/android/allaboutrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/android/allaboutrecyclerview/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.android.allaboutrecyclerview;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -27,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
     private RelativeLayout rlNoDataView;
 
     private static final String TAG = "MainActivity";
-
     private static final int NETWORK_DELAY = 3000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +45,66 @@ public class MainActivity extends AppCompatActivity {
         rvPirateList = findViewById(R.id.rv_ship_list);
         progressBar = findViewById(R.id.progress_bar);
         rlNoDataView = findViewById(R.id.rl_no_data);
-
         setupList();
+    }
+
+    /**
+     * todo 4
+     * Here we will inflate the created menu. This menu contains
+     * searchView. We need to set the SearchableInfo for searchView and also
+     * add a textChangedListener to listen to query changes.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.search_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
+        if (searchManager == null){
+            throw new RuntimeException("How can it be null?");
+        }
+
+        ComponentName componentName = new ComponentName(this, MainActivity.class);
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(componentName);
+
+        searchView.setSearchableInfo(searchableInfo);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //  Whenever the text for query is changes, we get a callback.
+                handleSearch(newText);
+                return false;
+            }
+        });
+
+//      https://stackoverflow.com/questions/37919328/searchview-hint-not-showing
+        searchView.setQueryHint(getString(R.string.hint_search_pirates));
+
+        return true;
+    }
+
+
+    //  Here we handle the search query. We check if
+    //  any ship name or captain name matches our query.
+    //  if yes we create a separate list of those ships
+    //  and update it in adapter.
+    private void handleSearch(String searchQuery) {
+        List<Ship> searchedShips = new ArrayList<>();
+        for (Ship ship : ships) {
+            if (Utils.isShipAssociatedWith(searchQuery, ship)){
+                searchedShips.add(ship);
+            }
+        }
+        adapter.setShips(searchedShips);
     }
 
     private void setupList(){
---
+++ app/src/main/java/com/example/android/allaboutrecyclerview/Utils.java
@@ -0,0 +1,32 @@
+package com.example.android.allaboutrecyclerview;
+
+import android.util.Log;
+
+import com.example.android.allaboutrecyclerview.data.models.Ship;
+
+/**
+ * Created by sahil-mac on 23/04/18.
+ */
+
+public final class Utils {
+
+    public static boolean checkWithAllPossibleCases(String query, String value) {
+
+        String smallQuery = query.toLowerCase();
+        String capsQuery = query.toUpperCase();
+        String capitalizeQuery = "";
+
+        if (query.length() > 0) {
+            capitalizeQuery = query.substring(0,1).toUpperCase()+query.substring(1);
+        }
+
+        return value.contains(smallQuery) || value.contains(capsQuery) || value.contains(capitalizeQuery);
+    }
+
+    public static boolean isShipAssociatedWith(String query, Ship ship) {
+
+        return checkWithAllPossibleCases(query, ship.name) ||
+                checkWithAllPossibleCases(query, ship.captain);
+
+    }
+}
